7f97d706d18a090205b8f90943d29f86c1fb6c7d
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-09-19 10:20:39 +0300

    Added Node.js REST API Template.

---
+++ JavaScript/Node.js REST API Template/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+require('./database/database')();
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.use('/auth', authRoutes);
+
+// General error handling
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+  next();
+})
+
+app.listen(port, () => { console.log(`REST API listening on port: ${port}...`) });
